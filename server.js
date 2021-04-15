bb5b8820e077db6561bfd1cd31044d7263960a1e
vinitha <selvivini@gmail.com>
2021-04-15 18:02:14 -0500

    added the connecting settings to server.js file

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-   useUnifiedTopology: true  
+  useCreateIndex: true,
+  useUnifiedTopology: true  
 });
 
 // routes
